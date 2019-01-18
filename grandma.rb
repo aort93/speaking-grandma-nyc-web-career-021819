df5e9e7624fb625aeba1cc25234b9685dc34d839
Alonso Ortega <alonsojortega@gmail.com>
2019-01-18 22:07:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,6 +15,8 @@
 def speak_to_grandma(str)
   if str != str.upcase
     return "HUH?! SPEAK UP, SONNY!"
+  elsif str == 'I LOVE YOU GRANDMA!'
+    return 'I LOVE YOU TOO PUMPKIN!'
   else
     return "NO, NOT SINCE 1938!"
   end
